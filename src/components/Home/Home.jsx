3ec01cf306097f982c7b5ab743ae72ca95979805
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-12-03 18:01:36 +0600

    category products added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Category from "../Category/Category";
+import Products from "../Products/Products";
 import Banner from "./Banner/Banner";
 import "./Home.scss";
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
       <div className="main-content">
         <div className="layout">
           <Category />
+          <Products/>
         </div>
       </div>
     </div>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,22 @@
+import Product from "./Product/Product";
 import "./Products.scss";
 const Products = () => {
-    return <div>Products</div>;
+  return (
+    <div className="products-container">
+      <div className="sec-heading">Section heading</div>
+      <div className="products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
 };
 
 export default Products;
