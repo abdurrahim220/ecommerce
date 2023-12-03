7c38f49acaa1b279a2c4af24d54e4d7b6b4af279
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-12-03 11:19:48 +0600

    newsletter done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,13 +3,14 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import AppContext from "../utils/context";
-
+import Newsletter from "../components/Footer/Newsletter/Newsletter";
 
 const Main = () => {
   return (
     <AppContext>
       <Header />
       <Outlet />
+      <Newsletter />
       <Footer />
     </AppContext>
   );
