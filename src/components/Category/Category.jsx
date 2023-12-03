c70bb83ccc22089f830d80ea94aa19d37f4a4e3f
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-12-03 16:54:00 +0600

    category added

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,6 +1,29 @@
 import "./Category.scss";
+
+import cat1 from "../../assets/category/cat-1.jpg";
+import cat2 from "../../assets/category/cat-2.jpg";
+import cat3 from "../../assets/category/cat-3.jpg";
+import cat4 from "../../assets/category/cat-4.jpg";
+
 const Category = () => {
-    return <div>Category</div>;
+  return (
+    <div className="shop-by-category">
+      <div className="categories">
+        <div className="category">
+          <img  alt="" src={cat1} />
+        </div>
+        <div className="category">
+          <img  alt="" src={cat2} />
+        </div>
+        <div className="category">
+          <img  alt="" src={cat3} />
+        </div>
+        <div className="category">
+          <img  alt="" src={cat4} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
