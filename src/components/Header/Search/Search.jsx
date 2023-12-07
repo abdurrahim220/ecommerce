33bdd3b65149754426bd9359fafbe3453e4bd44b
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-12-07 18:45:24 +0600

    search implement done

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,24 +1,69 @@
 import { MdClose } from "react-icons/md";
 import "./Search.scss";
 import prod from "../../../assets/products/earbuds-prod-1.webp";
+import { useNavigate } from "react-router-dom";
+import useProducts from "../../../hooks/useProducts";
+import React, { useState } from "react";
+
 const Search = ({ setShowSearch }) => {
+  const { data2 } = useProducts(`/products`);
+  // console.log(data2)
+  const [query, setQuery] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(null);
+  const navigate = useNavigate();
+
+  const filterProducts = () => {
+    if (query.trim() === "") {
+      setFilteredProducts(null);
+    } else {
+      const filtered = data2?.filter((product) => {
+        return product.title.toLowerCase().includes(query.toLowerCase());
+      });
+      setFilteredProducts(filtered);
+    }
+  };
+
+  // Update filtered products whenever the query changes
+  React.useEffect(() => {
+    filterProducts();
+  }, [query, data2]);
+
   return (
     <div className="search-modal">
       <div className="form-field">
-        <input type="text" autoFocus placeholder="Search for products" />
+        <input
+          type="text"
+          autoFocus
+          placeholder="Search for products"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
         <MdClose onClick={() => setShowSearch(false)} />
       </div>
       <div className="search-result-content">
         <div className="search-results">
-          <div className="search-result-item">
-            <div className="image-container">
-              <img src={prod} alt="" />
-            </div>
-            <div className="prod-details">
-              <span className="name">product name</span>
-              <span className="desc">product desc</span>
-            </div>
-          </div>
+          {filteredProducts === null ? (
+            <div>No results</div>
+          ) : (
+            filteredProducts?.map((product) => (
+              <div
+                onClick={() => {
+                  navigate(`/product/${product._id}`);
+                  setShowSearch(false);
+                }}
+                className="search-result-item"
+                key={product.id}
+              >
+                <div className="image-container">
+                  <img src={product.image} alt={product.title} />
+                </div>
+                <div className="prod-details">
+                  <span className="name">{product.title}</span>
+                  <span className="desc">{product.description}</span>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </div>
